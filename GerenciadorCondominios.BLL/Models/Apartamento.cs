84b67478434e62f59864a0e5bdc3f45bdef018ec
Thallison Morais <t.morais@reply.com>
2021-09-09 14:12:51 -0300

    Projeto gerenciador de condominios

---
+++ GerenciadorCondominios.BLL/Models/Apartamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GerenciadorCondominios.BLL.Models
+{
+    public class Apartamento
+    {
+        public int ApartamentoId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inválido")]
+        [Display(Name = "Número")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inválido")]
+        public int Andar { get; set; }
+
+        public string Foto { get; set; }
+
+        public string MoradorId { get; set; }
+
+        public virtual Usuario Morador { get; set; }
+
+        public string ProprietarioId { get; set; }
+
+        public virtual Usuario Proprietario { get; set; }
+    }
+}
---
+++ GerenciadorCondominios.DAL/Mapeamentos/ServicoPredioMap.cs
@@ -0,0 +1,23 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominios.DAL.Mapeamentos
+{
+    public class ServicoPredioMap : IEntityTypeConfiguration<ServicoPredio>
+    {
+        public void Configure(EntityTypeBuilder<ServicoPredio> builder)
+        {
+            builder.HasKey(sp => sp.ServicoId);
+            builder.Property(sp => sp.ServicoId).IsRequired();
+            builder.Property(sp => sp.DataExecucao).IsRequired();
+
+            builder.HasOne(sp => sp.Servico).WithMany(sp => sp.ServicosPredios).HasForeignKey(sp => sp.ServicoId);
+
+            builder.ToTable("ServicoPredios");
+        }
+    }
+}
